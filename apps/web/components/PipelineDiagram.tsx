f6b530b313a2fa79cc26e2fccb4223d5cc181aef
ibrahimapena1505 <ibrahim.apena@gmail.com>
2025-10-01 11:34:28 +0200

    Update PipelineDiagram.tsx
    Change 
    import Grid2 from '@mui/material/Unstable_Grid2';
    to 
    import Grid2 from '@mui/material/Grid2';

--- apps/web/components/PipelineDiagram.tsx
+++ apps/web/components/PipelineDiagram.tsx
@@ -4,7 +4,7 @@ import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Divider from '@mui/material/Divider';
-import Grid2 from '@mui/material/Unstable_Grid2';
+import Grid2 from '@mui/material/Grid2';
 import Typography from '@mui/material/Typography';
 
 const PIPELINE = [
